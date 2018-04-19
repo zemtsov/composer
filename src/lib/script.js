3a27133d34521b632bec717ab7c281f970ecb24f
ALEXANDER ZEMTSOV <alexander.zemtsov@ru.ibm.com>
2018-04-19 21:33:33 +0300

    Back to promises for 0.16

--- src/lib/script.js
+++ src/lib/script.js
@@ -3,7 +3,7 @@
  * @param {org.sample.ProposeTrade} tx
  * @transaction
  */
-async function onProposeTrade(tx) {
+function onProposeTrade(tx) {
 
   // check if the animal has correct status
   if (tx.animal.movementStatus !== 'IN_FIELD') {
@@ -16,6 +16,10 @@ async function onProposeTrade(tx) {
     throw new Error('This is not your animal!')
   }
 
+  if (tx.price <= 0) {
+    throw new Error('Invalid price')
+  }
+
   // change the movement status of the animal
   tx.animal.movementStatus = 'IN_TRANSIT';
 
@@ -23,10 +27,11 @@ async function onProposeTrade(tx) {
   tx.animal.proposedPrice = tx.price;
 
   // get Animals registry
-  const ar = await getAssetRegistry('org.sample.Animal');
-
-  // update the Animal in the registry
-  await ar.update(tx.animal)
+  return getAssetRegistry('org.sample.Animal')
+    .then(function (ar) {
+      // update the Animal in the registry
+      return ar.update(tx.animal);
+    })
 }
 
 //////////////////////////////////////////////////////////////////////
@@ -34,7 +39,10 @@ async function onProposeTrade(tx) {
  * @param {org.sample.AcceptTrade} tx
  * @transaction
  */
-async function onAcceptTrade(tx) {
+function onAcceptTrade(tx) {
+
+  var farmer;
+  var farmerRegistry;
 
   // check if the animal has correct status
   if (tx.animal.movementStatus !== 'IN_TRANSIT') {
@@ -42,30 +50,39 @@ async function onAcceptTrade(tx) {
   }
 
   // get farmers registry
-  const fr = await getParticipantRegistry('org.sample.Farmer');
-  // find the proper farmer in the registry
-  const farmer = await fr.get(getCurrentParticipant().getIdentifier());
+  return getParticipantRegistry('org.sample.Farmer')
+    .then(function (fr) {
+      farmerRegistry = fr;
+      // find the proper farmer in the registry
+      return fr.get(getCurrentParticipant().getIdentifier());
+    })
+    .then(function (f) {
+      farmer = f;
 
-  if (farmer.balance < tx.animal.proposedPrice) {
-    throw new Error("Not enough money on your balance")
-  }
+      if (farmer.balance < tx.animal.proposedPrice) {
+        throw new Error("Not enough money on your balance")
+      }
 
-  // reduce the farmer's balance
-  farmer.balance -= tx.animal.proposedPrice;
-  // change the animal's status
-  tx.animal.movementStatus = 'IN_FIELD';
-  // change the animal's owner
-  tx.animal.owner = farmer;
-  // zero the proposed price
-  tx.animal.proposedPrice = 0.0;
+      // reduce the farmer's balance
+      farmer.balance -= tx.animal.proposedPrice;
+      // change the animal's status
+      tx.animal.movementStatus = 'IN_FIELD';
+      // change the animal's owner
+      tx.animal.owner = farmer;
+      // zero the proposed price
+      tx.animal.proposedPrice = 0.0;
 
-  // update farmer in the registry
-  await fr.update(farmer);
-
-  // get animals registry
-  const ar = await getAssetRegistry('org.sample.Animal');
-  // update the animal in the registry
-  await ar.update(tx.animal);
+      // update farmer in the registry
+      return farmerRegistry.update(farmer);
+    })
+    .then(function () {
+      // get animals registry
+      return getAssetRegistry('org.sample.Animal');
+    })
+    .then(function (ar) {
+      // update the animal in the registry
+      return ar.update(tx.animal);
+    });
 }
 
 
@@ -74,7 +91,7 @@ async function onAcceptTrade(tx) {
  * @param {org.sample.CancelTrade} tx
  * @transaction
  */
-async function onCancelTrade(tx) {
+function onCancelTrade(tx) {
 
   // check if the animal has correct status
   if (tx.animal.movementStatus !== 'IN_TRANSIT') {
@@ -92,9 +109,11 @@ async function onCancelTrade(tx) {
   // zero the proposed price
   tx.animal.proposedPrice = 0.0;
 
-  const ar = await getAssetRegistry('org.sample.Animal');
-  // update the animal in the registry
-  await ar.update(tx.animal);
+  return getAssetRegistry('org.sample.Animal')
+    .then(function (ar) {
+      // update the animal in the registry
+      return ar.update(tx.animal);
+    });
 }
 
 
@@ -103,14 +122,10 @@ async function onCancelTrade(tx) {
  * @param {org.sample.SetupDemo} setupDemo
  * @transaction
  */
-async function setupDemo(setupDemo) {
+function setupDemo(setupDemo) {
 
-  const factory = getFactory();
-  const NS = 'org.sample';
-
-  // get registries
-  const farmerRegistry = await getParticipantRegistry(NS + '.Farmer');
-  const animalRegistry = await getAssetRegistry(NS + '.Animal');
+  var factory = getFactory();
+  var NS = 'org.sample';
 
   // create farmers
   var farmers = [
@@ -132,86 +147,94 @@ async function setupDemo(setupDemo) {
     factory.newResource(NS, 'Animal', 'animal9'),
   ];
 
-  // fill farmers with basic info
-  farmers[0].firstName = 'Bartholomew';
-  farmers[0].lastName = 'Carley';
-  farmers[0].balance = 100.0;
+  // get farmers registries
+  return getParticipantRegistry(NS + '.Farmer')
+    .then(function (farmerRegistry) {
+      // fill farmers with basic info
+      farmers[0].firstName = 'Bartholomew';
+      farmers[0].lastName = 'Carley';
+      farmers[0].balance = 100.0;
 
-  farmers[1].firstName = 'Chadwick';
-  farmers[1].lastName = 'Cherokee';
-  farmers[1].balance = 100.0;
+      farmers[1].firstName = 'Chadwick';
+      farmers[1].lastName = 'Cherokee';
+      farmers[1].balance = 100.0;
 
-  farmers[2].firstName = 'Franklin';
-  farmers[2].lastName = 'Harcourt';
-  farmers[2].balance = 100.0;
+      farmers[2].firstName = 'Franklin';
+      farmers[2].lastName = 'Harcourt';
+      farmers[2].balance = 100.0;
 
-  // write farmers to the registry
-  await farmerRegistry.addAll(farmers);
+      // write farmers to the registry
+      return farmerRegistry.addAll(farmers);
+    })
+    .then(function () {
+      return getAssetRegistry(NS + '.Animal');
+    })
+    .then(function (animalRegistry) {
+      // fill animals with basic info
+      animals[0].species = 'SHEEP';
+      animals[0].name = 'Molly';
+      animals[0].colour = 'White';
+      animals[0].movementStatus = 'IN_FIELD';
+      animals[0].productionType = 'MEAT';
+      animals[0].owner = factory.newRelationship(NS, 'Farmer', farmers[0].email);
 
-  // fill animals with basic info
-  animals[0].species = 'SHEEP';
-  animals[0].name = 'Molly';
-  animals[0].colour = 'White';
-  animals[0].movementStatus = 'IN_FIELD';
-  animals[0].productionType = 'MEAT';
-  animals[0].owner = factory.newRelationship(NS, 'Farmer', farmers[0].email);
+      animals[1].species = 'CATTLE';
+      animals[1].name = 'Dolly';
+      animals[1].colour = 'Black';
+      animals[1].movementStatus = 'IN_FIELD';
+      animals[1].productionType = 'WOOL';
+      animals[1].owner = factory.newRelationship(NS, 'Farmer', farmers[1].email);
 
-  animals[1].species = 'CATTLE';
-  animals[1].name = 'Dolly';
-  animals[1].colour = 'Black';
-  animals[1].movementStatus = 'IN_FIELD';
-  animals[1].productionType = 'WOOL';
-  animals[1].owner = factory.newRelationship(NS, 'Farmer', farmers[1].email);
+      animals[2].species = 'SHEEP';
+      animals[2].name = 'Larry';
+      animals[2].colour = 'Red';
+      animals[2].movementStatus = 'IN_FIELD';
+      animals[2].productionType = 'DAIRY';
+      animals[2].owner = factory.newRelationship(NS, 'Farmer', farmers[2].email);
 
-  animals[2].species = 'SHEEP';
-  animals[2].name = 'Larry';
-  animals[2].colour = 'Red';
-  animals[2].movementStatus = 'IN_FIELD';
-  animals[2].productionType = 'DAIRY';
-  animals[2].owner = factory.newRelationship(NS, 'Farmer', farmers[2].email);
+      animals[3].species = 'CATTLE';
+      animals[3].name = 'Sven';
+      animals[3].colour = 'Blue';
+      animals[3].movementStatus = 'IN_FIELD';
+      animals[3].productionType = 'BREEDING';
+      animals[3].owner = factory.newRelationship(NS, 'Farmer', farmers[0].email);
 
-  animals[3].species = 'CATTLE';
-  animals[3].name = 'Sven';
-  animals[3].colour = 'Blue';
-  animals[3].movementStatus = 'IN_FIELD';
-  animals[3].productionType = 'BREEDING';
-  animals[3].owner = factory.newRelationship(NS, 'Farmer', farmers[3].email);
+      animals[4].species = 'SHEEP';
+      animals[4].name = 'Polo';
+      animals[4].colour = 'Green';
+      animals[4].movementStatus = 'IN_FIELD';
+      animals[4].productionType = 'MEAT';
+      animals[4].owner = factory.newRelationship(NS, 'Farmer', farmers[1].email);
 
-  animals[4].species = 'SHEEP';
-  animals[4].name = 'Polo';
-  animals[4].colour = 'Green';
-  animals[4].movementStatus = 'IN_FIELD';
-  animals[4].productionType = 'MEAT';
-  animals[4].owner = factory.newRelationship(NS, 'Farmer', farmers[4].email);
+      animals[5].species = 'SHEEP';
+      animals[5].name = 'Priscilla'
+      animals[5].colour = 'Red';
+      animals[5].movementStatus = 'IN_FIELD';
+      animals[5].productionType = 'WOOL';
+      animals[5].owner = factory.newRelationship(NS, 'Farmer', farmers[2].email);
 
-  animals[5].species = 'SHEEP';
-  animals[5].name = 'Priscilla'
-  animals[5].colour = 'Red';
-  animals[5].movementStatus = 'IN_FIELD';
-  animals[5].productionType = 'WOOL';
-  animals[5].owner = factory.newRelationship(NS, 'Farmer', farmers[0].email);
+      animals[6].species = 'CATTLE';
+      animals[6].name = 'Jack';
+      animals[6].colour = 'White';
+      animals[6].movementStatus = 'IN_FIELD';
+      animals[6].productionType = 'BREEDING';
+      animals[6].owner = factory.newRelationship(NS, 'Farmer', farmers[0].email);
 
-  animals[6].species = 'CATTLE';
-  animals[6].name = 'Jack';
-  animals[6].colour = 'White';
-  animals[6].movementStatus = 'IN_FIELD';
-  animals[6].productionType = 'BREEDING';
-  animals[6].owner = factory.newRelationship(NS, 'Farmer', farmers[1].email);
+      animals[7].species = 'PIG';
+      animals[7].name = 'Coribda';
+      animals[7].colour = 'Blue';
+      animals[7].movementStatus = 'IN_FIELD';
+      animals[7].productionType = 'DAIRY';
+      animals[7].owner = factory.newRelationship(NS, 'Farmer', farmers[1].email);
 
-  animals[7].species = 'PIG';
-  animals[7].name = 'Coribda';
-  animals[7].colour = 'Blue';
-  animals[7].movementStatus = 'IN_FIELD';
-  animals[7].productionType = 'DAIRY';
-  animals[7].owner = factory.newRelationship(NS, 'Farmer', farmers[2].email);
+      animals[8].species = 'SHEEP';
+      animals[8].name = 'Mickey';
+      animals[8].colour = 'Green';
+      animals[8].movementStatus = 'IN_FIELD';
+      animals[8].productionType = 'WOOL';
+      animals[8].owner = factory.newRelationship(NS, 'Farmer', farmers[2].email);
 
-  animals[8].species = 'SHEEP';
-  animals[8].name = 'Mickey';
-  animals[8].colour = 'Green';
-  animals[8].movementStatus = 'IN_FIELD';
-  animals[8].productionType = 'WOOL';
-  animals[8].owner = factory.newRelationship(NS, 'Farmer', farmers[3].email);
-
-  // write animals to the registry
-  await animalRegistry.addAll(animals);
+      // write animals to the registry
+      return animalRegistry.addAll(animals);
+    });
 }
